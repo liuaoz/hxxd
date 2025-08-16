62db6346e52dcd59a1a7d3f829b05c8823ee532c
matrix <576729125@qq.com>
2025-08-16 18:35:37 +0800

    x

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,4 @@
 
 docker build -t hxxd .
 
-docker run -d --name hxxd -p 8080:8080 --env-file hxxd.env hxxd
+docker run -d --name hxxd -p 8080:8080 -v /data/cert/wx_cert:/data/cert --env-file hxxd.env hxxd
